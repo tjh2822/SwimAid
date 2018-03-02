9197e1acdbc7ec1e2acea84da75ff775919a13df
Thomas J. Hall <tjh2822@g.rit.edu>
2018-03-02 14:21:57 -0500

    calibrated (almost) code, ready to begin velocity calculations

--- SwimAid.ino
+++ SwimAid.ino
@@ -6,32 +6,94 @@
  * note:vcc-->5v ,but ADXL335 Vs is 3.3V
  * The circuit:
  *       5V: VCC
- * analog 1: x-axis
- * analog 2: y-axis
- * analog 3: z-axis
 */
 
-//connect 3.3v to AREF
-const int xPin = A1;  // our x pin
-const int yPin = A2;  // our y pin
-const int zPin = A3;  // our x pin
+#include <Wire.h>
+
+long accelX, accelY, accelZ;      // longs for the raw accelrometer data
+float gForceX, gForceY, gForceZ;  // floats for the data processed in gs
+
+long gyroX, gyroY, gyroZ;         // longs for the raw accelorometer data
+float rotX, rotY, rotZ;           // floats for the data processed
 
 void setup() {
-  // initialize serial communications at 9600 bps:
-  Serial.begin(9600);
+  Serial.begin(9600);             // set up serial for debugging purposses
+  Wire.begin();                   // begin wire so we can connect to the correct registers for the data
+  setupMPU();                     // set up the MPU accelrometer and gyroscope
 }
 
-void loop() {
-  
-  Serial.print(analogRead(xPin)-115);
-  Serial.print("\t");
-  
-  Serial.print(analogRead(yPin)-120);
-  Serial.print("\t");
-  
-  Serial.print(analogRead(zPin)-110);
-  Serial.println();
 
-  delay(10);                     
-  
+void loop() {
+  recordAccelRegisters();         // get the data
+  recordGyroRegisters();          // get the data
+  printData();                    // print said data
+  delay(100);                     // wait a tenth of a second
+}
+
+void setupMPU(){
+  Wire.beginTransmission(0b1101000); //This is the I2C address of the MPU (b1101000/b1101001 for AC0 low/high datasheet sec. 9.2)
+  Wire.write(0x6B); //Accessing the register 6B - Power Management (Sec. 4.28)
+  Wire.write(0b00000000); //Setting SLEEP register to 0. (Required; see Note on p. 9)
+  Wire.endTransmission();  
+  Wire.beginTransmission(0b1101000); //I2C address of the MPU
+  Wire.write(0x1B); //Accessing the register 1B - Gyroscope Configuration (Sec. 4.4) 
+  Wire.write(0x00000000); //Setting the gyro to full scale +/- 250deg./s 
+  Wire.endTransmission(); 
+  Wire.beginTransmission(0b1101000); //I2C address of the MPU
+  Wire.write(0x1C); //Accessing the register 1C - Acccelerometer Configuration (Sec. 4.5) 
+  Wire.write(0b00000000); //Setting the accel to +/- 2g
+  Wire.endTransmission(); 
+}
+
+void recordAccelRegisters() {
+  Wire.beginTransmission(0b1101000); //I2C address of the MPU
+  Wire.write(0x3B); //Starting register for Accel Readings
+  Wire.endTransmission();
+  Wire.requestFrom(0b1101000,6); //Request Accel Registers (3B - 40)
+  while(Wire.available() < 6);
+  accelX = Wire.read()<<8|Wire.read(); //Store first two bytes into accelX
+  accelY = Wire.read()<<8|Wire.read(); //Store middle two bytes into accelY
+  accelZ = Wire.read()<<8|Wire.read(); //Store last two bytes into accelZ
+  processAccelData();
+}
+
+void processAccelData(){
+  gForceX = accelX / 16384.0;
+  gForceY = accelY / 16384.0; 
+  gForceZ = accelZ / 16384.0;
+}
+
+void recordGyroRegisters() {
+  Wire.beginTransmission(0b1101000); //I2C address of the MPU
+  Wire.write(0x43); //Starting register for Gyro Readings
+  Wire.endTransmission();
+  Wire.requestFrom(0b1101000,6); //Request Gyro Registers (43 - 48)
+  while(Wire.available() < 6);
+  gyroX = Wire.read()<<8|Wire.read(); //Store first two bytes into accelX
+  gyroY = Wire.read()<<8|Wire.read(); //Store middle two bytes into accelY
+  gyroZ = Wire.read()<<8|Wire.read(); //Store last two bytes into accelZ
+  processGyroData();
+}
+
+void processGyroData() {
+  rotX = gyroX / 131.0;
+  rotY = gyroY / 131.0; 
+  rotZ = gyroZ / 131.0;
+}
+
+void printData() {
+  Serial.print("Gyro (deg)");
+  Serial.print(" X=");
+  Serial.print(rotX);
+  Serial.print(" Y=");
+  Serial.print(rotY);
+  Serial.print(" Z=");
+  Serial.print(rotZ);
+  Serial.print(" Accel (g)");
+  Serial.print(" X=");
+  Serial.print(gForceX);
+  Serial.print(" Y=");
+  Serial.print(gForceY);
+  Serial.print(" Z=");
+  Serial.println(gForceZ);
 }
